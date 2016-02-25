7150a9ad3d0970bc41ded4c253d588c385f5f4b9
Nicolas <lecourtoisn@gmail.com>
2016-02-25 01:21:10 +0100

    Mutant testing and report generation works

--- MutationPlugin/src/main/java/MutantTesting.java
+++ MutationPlugin/src/main/java/MutantTesting.java
@@ -1,3 +1,4 @@
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -7,11 +8,31 @@ import org.apache.maven.plugins.annotations.Mojo;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 @Mojo(name = "testing")
 public class MutantTesting extends AbstractMojo {
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        System.out.println("I'm testing mojo");
+        File mutantRootPath = new File("target/spooned/");
+        File compiledClassDir = new File("target/test-classes");
+
+        File[] files = mutantRootPath.listFiles();
+        if (files == null) {
+            System.out.println("No mutants to test");
+            return;
+        }
+        for (File file : files) {
+            File spoonTestClassDir = new File(file.getPath().concat("/target/"));
+            try {
+                FileUtils.copyDirectoryToDirectory(compiledClassDir, spoonTestClassDir);
+                FileUtils.copyFileToDirectory(new File("pom.xml"), file);
+
+                Runtime.getRuntime().exec("cmd.exe /c mvn surefire:test", null, file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
