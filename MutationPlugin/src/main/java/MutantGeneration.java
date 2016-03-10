a259a59674f7a39e1199b365d93be9daff8dc123
Nicolas <lecourtoisn@gmail.com>
2016-03-10 20:41:42 +0100

    Add a bit of structure in generate mojo

--- MutationPlugin/src/main/java/MutantGeneration.java
+++ MutationPlugin/src/main/java/MutantGeneration.java
@@ -1,16 +1,15 @@
+import generator.MutantGenerator;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import processors.*;
-import spoon.Launcher;
-import spoon.SpoonModelBuilder;
-import spoon.reflect.declaration.ModifierKind;
+import spoon.processing.Processor;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Mojo(name = "generate")
 public class MutantGeneration extends AbstractMojo{
@@ -31,58 +30,32 @@ public class MutantGeneration extends AbstractMojo{
     @Parameter (property = "generate.myReturn", defaultValue = "true")
     private boolean myReturn;
 
+    public Set<CustomProcessor> retrieveProcessors() {
+        Set<CustomProcessor> processors = new HashSet<CustomProcessor>();
+        processors.addAll(LogicOperatorProcessor.getSomeCouples(logicOperator));
+        processors.addAll(BinaryOperatorProcessor.getSomeCouples(binaryOperator));
+        processors.addAll(OperatorProcessor.getSomeCouples(operator));
+        processors.addAll(ModifierProcessor.getSomeCouples(modifier));
+        if (myReturn) processors.add(new ReturnProcessor());
+        return processors;
+    }
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        //Liste des diff�rentes mutations
-        List<CustomProcessor> processorList = new ArrayList<CustomProcessor>();
-        //processorList.add(new RemoveConstructorProcessor());
-        processorList.addAll(LogicOperatorProcessor.getSomeCouples(logicOperator));
-        processorList.addAll(BinaryOperatorProcessor.getSomeCouples(binaryOperator));
-        processorList.addAll(OperatorProcessor.getSomeCouples(operator));
-        processorList.addAll(ModifierProcessor.getSomeCouples(modifier));
-        if (myReturn) processorList.add(new ReturnProcessor());
+        // Processors that are to be used by spoon
+        Set<CustomProcessor> processorSet = retrieveProcessors();
+        File mutantsRootPath = new File("target/spooned/");
 
-        int i = 0;
+        //int i = 0;
+        for (CustomProcessor proc : processorSet) {
+            MutantGenerator generator = new MutantGenerator(this.getLog(), proc, mutantsRootPath);
 
-        for (CustomProcessor proc : processorList) {
-            Launcher api = new Launcher();
-
-            String mutantName = proc.getClass().getSimpleName();
-            File mutantsRootPath = new File("target/spooned/");
-
-            File mutantProject = new File(mutantsRootPath.getPath().concat("/" + mutantName + i));
-
-            File mutantClasses = new File(mutantProject.getPath().concat("/target/classes"));
-            File mutantSources = new File(mutantProject.getPath().concat("/src/main/java"));
-
-            // The processor mutating the project is specified
-            api.addProcessor(proc);
-            // The source code to alter is specified too
-            api.addInputResource("./src/main/java/");
-
-            // The directories in which the sources will be generated and compiled are set
-            api.setSourceOutputDirectory(mutantSources.getPath());
-            api.setBinaryOutputDirectory(mutantClasses.getPath());
-
-            // Spoon runs and generate the new sources
-            api.run();
-
-            SpoonModelBuilder builder = api.getModelBuilder();
-
-            // ... and compile them
-
-
-            boolean compiled = false;
             try {
-                compiled = builder.compile();
+                generator.applyProcessor();
+                generator.compileMutants();
             } catch (Exception e) {
-                this.getLog().error("Uncompiled mutant : " + mutantName);
                 e.printStackTrace();
             }
-
-            proc.generateXml(mutantProject, compiled);
-
-            i++;
         }
     }
 }
---
+++ MutationPlugin/src/main/java/generator/MutantGenerator.java
@@ -0,0 +1,73 @@
+package generator;
+
+import org.apache.maven.plugin.logging.Log;
+import processors.CustomProcessor;
+import spoon.Launcher;
+import spoon.SpoonModelBuilder;
+
+import java.io.File;
+
+public class MutantGenerator {
+
+    private Launcher launcher;
+    private Log logger;
+    private CustomProcessor proc;
+    private File mutantProject;
+
+    private static int i = 0;
+
+    public MutantGenerator(Log logger, CustomProcessor processor, File mutantsRootPath) {
+        ++i;
+        this.logger = logger;
+        this.proc = processor;
+        this.launcher = new Launcher();
+
+        setMutantProject(mutantsRootPath);
+    }
+
+    public void applyProcessor() throws Exception {
+
+        File mutantClasses = new File(mutantProject.getPath().concat("/target/classes"));
+        File mutantSources = new File(mutantProject.getPath().concat("/src/main/java"));
+
+        // The proc mutating the project is specified
+        launcher.addProcessor(proc);
+        // The source code to alter is specified too
+        launcher.addInputResource("./src/main/java/");
+
+        // The directories in which the sources will be generated and compiled are set
+        launcher.setSourceOutputDirectory(mutantSources.getPath());
+        launcher.setBinaryOutputDirectory(mutantClasses.getPath());
+
+        // Spoon runs and generate the new sources
+
+        try {
+            launcher.run();
+        } catch (Exception e){
+            logger.error("Couldn't generate mutants with processor : " + mutantDirectory());
+            throw e;
+        }
+    }
+
+    public void compileMutants() throws Exception {
+        SpoonModelBuilder builder = launcher.getModelBuilder();
+
+        boolean compiled = false;
+        try {
+            compiled = builder.compile();
+        } catch (Exception e) {
+            logger.error("Uncompiled mutant : " + mutantDirectory());
+            throw e;
+        }
+
+        proc.generateXml(mutantProject, compiled);
+    }
+
+    public void setMutantProject(File mutantsRootPath) {
+        mutantProject = new File(mutantsRootPath.getPath().concat("/" + mutantDirectory()));
+    }
+
+    private String mutantDirectory() {
+        return proc.getClass().getSimpleName() + i;
+    }
+}
